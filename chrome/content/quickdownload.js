e5a66fecb50fc12571c73aa00155f58b9b2dac67
defagos <defagos@gmail.com>
2010-08-29 19:12:54 +0200

    Javascript source file added

---
+++ chrome/content/quickdownload.js
@@ -0,0 +1,17 @@
+var Quickdownload = {
+    init: function() {
+        alert("Init");
+    },
+
+    downloadFile: function() {
+        alert("Download file");
+    },
+    
+    downloadImage: function() {
+        alert("Download image");
+    },
+    
+    downloadBackgroundImage() {
+        alert("Download background Image");
+    }
+};
