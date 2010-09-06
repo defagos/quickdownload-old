acd43e9e242637300bda75f1ac16ffe192dc65f8
defagos <defagos@gmail.com>
2010-09-06 23:47:14 +0200

    Adding retrieval of download directory preferences

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,4 +1,9 @@
 // Default values for quickdownload preferences
-pref("quickdownload.fileDirectory", "");
-pref("quickdownload.imageDirectory", "");
-pref("quickdownload.backgroundImageDirectory", "");
+pref("quickdownload.fileDirIsDownloadDir", false);
+pref("quickdownload.fileDir", "");
+
+pref("quickdownload.imageDirIsDownloadDir", false);
+pref("quickdownload.imageDir", "");
+
+pref("quickdownload.backgroundImageDirIsDownloadDir", false);
+pref("quickdownload.backgroundImageDir", "");
